81e437161ef54f820c7926f1966e7a283b3bec08
Siniiiza <insidetennis@hotmail.com>
2025-06-11 14:28:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Открытие модалки
+document.getElementById('openChat').addEventListener('click', function() {
+  document.getElementById('chatModal').style.display = 'block';
+});
 
+// Закрытие модалки
+document.getElementById('closeChat').addEventListener('click', function() {
+  document.getElementById('chatModal').style.display = 'none';
+});
+
+// Закрытие при клике вне окна
+window.addEventListener('click', function(event) {
+  const modal = document.getElementById('chatModal');
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
